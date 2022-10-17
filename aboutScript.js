4b29c232054a09ce6e1592e244dfb50502f873f4
Edward <Ecampbell3015@gmail.com>
2022-10-16 23:09:38 -0700

    start about section for devices under 1024px width

--- aboutScript.js
+++ aboutScript.js
@@ -29,6 +29,6 @@ const tween = KUTE.fromTo(
   '#blob1',
   { path: '#blob1'},
   { path: '#blob2'},
-  { repeat: 999, duration: 4000, yoyo: true}
+  { repeat: 999, duration: 6000, yoyo: true}
 )
 tween.start();
